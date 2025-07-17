5108dc755e75d7ff643680a6c57a07ecf202e328
mahmoudessam16 <mahmoudemourad29@gmail.com>
2025-07-17 16:34:14 +0300

    make endpoints to get all users and user by username

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiNotFoundResponse, ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get(':username')
+  @ApiOkResponse({ description: 'Public user profile returned.' })
+  @ApiNotFoundResponse({ description: 'User not found.' })
+  async getUserProfile(@Param('username') username: string) {
+    return this.usersService.findByUsername(username);
+  }
+
+  @Get()
+  @ApiOkResponse({ description: 'List of all users returned.' })
+  async getAllUsers() {
+    return this.usersService.findAll();
+  }
+}
