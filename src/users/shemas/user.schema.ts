19efa56b63ac0dfa7da603589f146c04427b219a
mvicsa <mvicsa@yahoo.com>
2025-07-20 01:51:21 +0300

    fix: remove duplicate import in user schema file

--- src/users/shemas/user.schema.ts
+++ src/users/shemas/user.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
-import { Document } from 'mongoose';
 import { ApiProperty } from '@nestjs/swagger';
 
 export type UserDocument = User & Document;
